023c18ef6532218f1ec704fb40aa3ed86f314e92
Matt Perkins <mperkins@redhat.com>
2016-07-18 22:34:54 -0400

    update to webpack 2 and egghead example

--- front/www/js/bundle.js
+++ front/www/js/bundle.js
@@ -11,16 +11,16 @@
 
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
-/******/ 			exports: {},
-/******/ 			id: moduleId,
-/******/ 			loaded: false
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
 /******/ 		};
 
 /******/ 		// Execute the module function
 /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
 
 /******/ 		// Flag the module as loaded
-/******/ 		module.loaded = true;
+/******/ 		module.l = true;
 
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
@@ -33,18 +33,36 @@
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
 
+/******/ 	// identity function for calling harmory imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+
+/******/ 	// define getter function for harmory exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		Object.defineProperty(exports, name, {
+/******/ 			configurable: false,
+/******/ 			enumerable: true,
+/******/ 			get: getter
+/******/ 		});
+/******/ 	};
+
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+
 /******/ 	// __webpack_public_path__
 /******/ 	__webpack_require__.p = "";
 
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
+/*!****************************!*\
+  !*** ./front/app/index.js ***!
+  \****************************/
 /***/ function(module, exports) {
 
-	console.log('Hello from index.js');
+eval("console.log('Hello from index.js');\n\n//////////////////\n// WEBPACK FOOTER\n// ./front/app/index.js\n// module id = 0\n// module chunks = 0\n\n//# sourceURL=webpack:///./front/app/index.js?");
 
 /***/ }
 /******/ ]);
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,22 @@
-var path = require('path'),
-    nodeModulesPath = path.resolve(__dirname, 'node_modules'),
-    buildPath = path.resolve(__dirname, 'front', 'www', 'js'),
-    mainPath = path.resolve(__dirname, 'front', 'app', 'index.js');
+const {resolve} = require('path');
 
+let buildPath = resolve(__dirname, 'front', 'www', 'js'),
+    mainPath  = resolve(__dirname, 'front', 'app', 'index.js');
 
-module.exports = {
-    entry: mainPath,
-    output: {
-        path: buildPath,
-        filename: "bundle.js"
+module.exports = env => {
+  return {
+    entry  : mainPath,
+    output : {
+      path    : buildPath,
+      filename: "bundle.js",
+      pathinfo: !env.prod,
     },
-    module: {
-        loaders: [{
-            test: /\.css$/,
-            loader: "style!css"
-        }]
+    devtool: env.prod ? 'source-map' : 'eval',
+    bail   : env.prod,
+    module : {
+      loaders: [
+        {test: /\.css$/, loader: "style!css"}
+      ]
     }
-};
+  }
+}
