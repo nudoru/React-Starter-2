5f58a87bb255aa31adec4b00f65680fb2475a585
Matt Perkins <mperkins@redhat.com>
2016-07-20 12:00:48 -0400

    Eslint rules, react, moar loaders

--- front/app/index.js
+++ front/app/index.js
@@ -1,2 +1,6 @@
-console.log('Hello from index.js!');
-console.log('blah blah');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render((<div>
+  <h1>Hello world!</h1>
+</div>), document.querySelector('#app'));
