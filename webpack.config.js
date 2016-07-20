40198dc75226c23845f66f368b66430032991251
Matt Perkins <mperkins@redhat.com>
2016-07-20 13:46:10 -0400

    hmr

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,39 @@
 const {resolve} = require('path');
-const webpack = require('webpack');
-const buildPath = resolve(__dirname, 'front', 'www', 'js');
+const webpack   = require('webpack');
+const buildPath = resolve(__dirname, 'front', 'www', 'js', 'app');
 const mainPath  = resolve(__dirname, 'front', 'app', 'index.js');
-const isProd = process.env.NODE_ENV === 'production';
-const isTest = process.env.NODE_ENV === 'test';
+const isProd    = process.env.NODE_ENV === 'production';
+const isTest    = process.env.NODE_ENV === 'test';
 
 module.exports = env => {
+  const removeEmpty = array => array.filter(i => !!i);
   return {
     entry  : {
       app   : mainPath,
       vendor: ['lodash']
     },
     output : {
-      path    : buildPath,
-      filename: "bundle.[name].js",
-      pathinfo: !env.prod,
+      path      : buildPath,
+      filename  : "bundle.[name].js",
+      publicPath: 'http://localhost:8080/js/app/'
     },
     devtool: env.prod ? 'source-map' : 'eval',
     bail   : env.prod,
     module : {
       loaders: [
-        {test: /\.js$/, loader: 'babel!eslint', exclude: /node_modules/},
+        {
+          test   : /\.js$/,
+          loader : 'babel!eslint',
+          exclude: ['/node_modules/', '/app/vendor/']
+        },
         {test: /\.css$/, loader: "style!css"}
       ]
     },
-    plugins: [
-      isTest ? undefined : new webpack.optimize.CommonsChunkPlugin({name: 'vendor'})
-    ].filter(p => !!p)
+    plugins: removeEmpty([
+      isTest ? undefined : new webpack.optimize.CommonsChunkPlugin({name: 'vendor'}),
+      isProd ? undefined : new webpack.DefinePlugin({
+        'process.env': {NODE_ENV: '"production"'}
+      })
+    ])
   }
 };
